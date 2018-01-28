1a3ff1b770da968aadde7b2147ff26e224c1a492
roy989898 <roy989898@gmail.com>
2018-01-28 22:21:30 +0800

    add attr xml

--- app/src/main/java/com/example/pomingpo/few_customview/customView/CircleInsideHaveTextView1.java
+++ app/src/main/java/com/example/pomingpo/few_customview/customView/CircleInsideHaveTextView1.java
@@ -1,6 +1,7 @@
 package com.example.pomingpo.few_customview.customView;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
@@ -17,12 +18,37 @@ import com.example.pomingpo.few_customview.R;
 
 public class CircleInsideHaveTextView1 extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private int size = 150;
     private int noPaddingmeasureWidth;
     private int noPaddingmeasureHeight;
+    // TODO: 2018/1/28 set the % by xml and code
+    private float presentage;
+    // TODO: 2018/1/28 set the stork by xml
+    private float storkWidth;
+    private float innerTextSize;
 
     public CircleInsideHaveTextView1(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.CircleInsideHaveTextView1,
+                0, 0);
+
+        try {
+            presentage = a.getInteger(R.styleable.CircleInsideHaveTextView1_percentage, 0);
+            storkWidth = a.getDimension(R.styleable.CircleInsideHaveTextView1_storkWidth, 30);
+            innerTextSize = a.getDimension(R.styleable.CircleInsideHaveTextView1_innerTextSize, 20);
+        } finally {
+            a.recycle();
+        }
+    }
+
+    public float getPresentage() {
+        return presentage;
+    }
+
+    public void setPresentage(float presentage) {
+        this.presentage = presentage;
+        invalidate();
     }
 
     @Override
@@ -30,12 +56,11 @@ public class CircleInsideHaveTextView1 extends View {
         super.onDraw(canvas);
 
 
-        float presentage = 95f;
         float formStartAngle = presentage / 100.0f * 360.0f;
-        int storkWidth = 30;
+
         paint.setStyle(Paint.Style.FILL);
 
-        size = noPaddingmeasureWidth;
+        int size = noPaddingmeasureWidth;
         int left = 0;
         int right = left + size;
         int top = 0;
@@ -54,14 +79,14 @@ public class CircleInsideHaveTextView1 extends View {
         paint.setColor(getResources().getColor(R.color.white));
         float cx = right + (left - right) / 2f;
         float cy = top + (bottom - top) / 2f;
-        int radius = (right - left - storkWidth) / 2;
+        float radius = (right - left - storkWidth*2) / 2f;
         canvas.drawCircle(cx, cy, radius, paint);
 
 
         String textToShow = presentage + "%";
         paint.setColor(getResources().getColor(R.color.dark));
         paint.setTextAlign(Paint.Align.CENTER);
-        paint.setTextSize(25);
+        paint.setTextSize(innerTextSize);
         Rect result = new Rect();
         paint.getTextBounds(textToShow, 0, textToShow.length(), result);
         float yOffset = result.bottom - result.top / 2f;
